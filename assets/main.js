5eee5ab18f89029888bd7ab8d9847bad6eb9ff5f
millos910 <emontero910@gmail.com>
2023-12-07 01:17:50 -0500

    slider

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,5 @@ const conteinerCartProducts=document.querySelector('.hidden-cart')
 btnCart.addEventListener('click',()=>{conteinerCartProducts.classList.toggle('hidden-cart')})
 
 
-const  btnSwitch=document.querySelector('.switch');
-btnSwitch.addEventListener('click',()=> {
-    document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active');
-})
+
+
